21089e07ebe3c64a1963bb02c9cec4ae4731730b
Your Name <you@example.com>
2024-09-24 08:44:36 +0600

    pushing history done

--- js/interact.js
+++ js/interact.js
@@ -1,23 +1,27 @@
-// const donateAmount = document.getElementById('donation-amount').value;
-// console.log(donateAmount);
-const modal = document.getElementById('my-modal')
 function getValueById(id) {
   return parseFloat(document.getElementById(id).value);
 }
 function getValueByIdTextField(id) {
   return parseFloat(document.getElementById(id).innerText);
 }
+function getInnerText(id) {
+  return document.getElementById(id).innerText;
+}
 document.getElementById("donate-button").addEventListener("click", function () {
   const donateAmount = getValueById("donation-amount");
   const totalDonation = getValueByIdTextField("total-donation-noakhali");
   const netBalance = getValueByIdTextField("net-balance");
-  const displayTotalDonation = donateAmount + totalDonation ;
-  const remainingBalance = netBalance - donateAmount ;
-  document.getElementById("total-donation-noakhali").innerText = displayTotalDonation;
+  const historyContent = document.getElementById("history-container");
+  const donatePlace = getInnerText("donation-title");
+  console.log(donatePlace);
+  const displayTotalDonation = donateAmount + totalDonation;
+  const remainingBalance = netBalance - donateAmount;
+  document.getElementById("total-donation-noakhali").innerText =
+    displayTotalDonation;
   document.getElementById("net-balance").innerText = remainingBalance;
-
-
-  
-
- 
+  historyContent.innerHTML = `<div class="border border-white rounded-lg p-4">
+   <p class="text-lg text-slate-500 font-medium">${new Date().toLocaleDateString()}</p>
+      <h2 class="text-xl text-slate-800 font-semibold "> ${displayTotalDonation} donated ${donatePlace}</h2>
+     
+    </div>`;
 });
